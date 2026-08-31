15d4f9afb2efe175fd7f2c99edee43d4cf791a20
Darien Gillespie <dsgillespie02@yahoo.com>
2026-08-30 20:32:40 -0400

    Fix NeoForge MenuScreens

--- neoforge/src/main/java/com/helliongames/hellionsapi/client/HellionsAPINeoForgeClient.java
+++ neoforge/src/main/java/com/helliongames/hellionsapi/client/HellionsAPINeoForgeClient.java
@@ -4,7 +4,6 @@ import com.helliongames.hellionsapi.registration.holders.ParticleDataHolder;
 import com.helliongames.hellionsapi.registration.registries.HellionsAPIParticleRegistry;
 import com.helliongames.hellionsapi.registration.registries.client.HellionsAPIEntityRendererRegistry;
 import com.helliongames.hellionsapi.registration.registries.client.HellionsAPIMenuScreenRegistry;
-import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.MenuAccess;
 import net.minecraft.core.particles.ParticleOptions;
@@ -51,8 +50,6 @@ public class HellionsAPINeoForgeClient {
     }
 
     private static <M extends AbstractContainerMenu, S extends AbstractContainerScreen<M> & MenuAccess<M>> void registerMenuScreen(RegisterMenuScreensEvent event, HellionsAPIMenuScreenRegistry.ScreenEntry<M, S> entry) {
-        @SuppressWarnings("unchecked")
-        MenuScreens.ScreenConstructor<M, S> constructor = (MenuScreens.ScreenConstructor<M, S>) entry.getScreenConstructor();
-        event.register(entry.getHolder().get(), constructor);
+        event.register(entry.getHolder().get(), entry.getScreenConstructor()::create);
     }
 }
